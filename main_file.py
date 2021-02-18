aa3c759ba6b71edd08fa1ba2cbf358d107036ecf
michaelba8 <63546875+michaelba8@users.noreply.github.com>
2021-02-18 17:18:17 +0200

    read images

--- main_file.py
+++ main_file.py
@@ -1,7 +1,27 @@
 import numpy as np
+import cv2
+import os
+import pathlib
+
+path=pathlib.Path().absolute()
 
 def main():
-    pass
+    images=read_images(path)
+    for img in images:
+        cv2.imshow('img',img)
+        cv2.waitKey()
 
+
+
+
+def read_images(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(filename,0)
+        if img is not None:
+            images.append(img)
+        else:
+            print(filename)
+    return images
 if __name__=='__main__':
     main()
